ee17aa84b56a9a9d0150f4fc3c79b43b82b1e5df
Doug <dmick92@gmail.com>
2024-07-22 18:04:49 -0400

    updated vite config

--- template/base/vue/vite.config.ts
+++ template/base/vue/vite.config.ts
@@ -4,15 +4,27 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist/app"
+  },
   plugins: [
     vue(),
     vueJsx(),
+    basicSsl()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    https: true,
+    host: '127.0.0.1',
+    port: 5000,
+  },
+  base: ''
 })
