e1bf26ffc422f8f97cfd7568a194dfbadb4d0ff3
Doug <dmick92@gmail.com>
2024-07-25 14:46:15 -0400

    Added tailwind support for vue

--- src/installers/tailwind.ts
+++ src/installers/tailwind.ts
@@ -2,10 +2,44 @@ import path from "path";
 import fs from "fs-extra";
 
 import { PKG_ROOT } from "~/consts.js";
-import { type Installer } from "~/installers/index.js";
+import { AvailableFrameworks, type Installer } from "~/installers/index.js";
 import { addPackageDependency } from "~/utils/addPackageDependency.js";
 
-export const tailwindInstaller: Installer = ({ projectDir }) => {
+const handleFramework = (projectDir: string, framework: AvailableFrameworks) => {
+  switch (framework) {
+    case "react": {
+      const mainFile = path.join(projectDir, "src/main.tsx");
+      fs.readFile(mainFile, "utf8", (err, data) => {
+        if (err) throw err;
+        const newData = data.replace("import './index.css'", `import './styles/globals.css'`);
+        fs.writeFileSync(mainFile, newData);
+      });
+      break;
+    }
+    case "vue": {
+      const mainFile = path.join(projectDir, "src/main.ts");
+      fs.readFile(mainFile, "utf8", (err, data) => {
+        if (err) throw err;
+        const newData = data.replace("import './assets/main.css'", `import './styles/globals.css'`);
+        fs.writeFileSync(mainFile, newData);
+      });
+
+      const twCfgDest = path.join(projectDir, "tailwind.config.ts");
+      fs.readFile(twCfgDest, "utf8", (err, data) => {
+        if (err) throw err;
+        const newData = data.replace("plugins: [],", `plugins: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],`);
+        fs.writeFileSync(twCfgDest, newData);
+      });
+      break;
+    }
+    default: {
+      throw new Error(`Unknown framework: ${framework}`);
+    }
+  }
+};
+
+export const tailwindInstaller: Installer = ({ projectDir, framework }) => {
   addPackageDependency({
     projectDir,
     dependencies: [
@@ -31,16 +65,10 @@ export const tailwindInstaller: Installer = ({ projectDir }) => {
   const cssSrc = path.join(extrasDir, "src/styles/globals.css");
   const cssDest = path.join(projectDir, "src/styles/globals.css");
 
-  const mainFile = path.join(projectDir, "src/main.tsx");
-
-  fs.readFile(mainFile, "utf8", (err, data) => {
-    if (err) throw err;
-    const newData = data.replace("import './index.css'", `import './styles/globals.css'`);
-    fs.writeFileSync(mainFile, newData);
-  });
-
   fs.copySync(twCfgSrc, twCfgDest);
   fs.copySync(postcssCfgSrc, postcssCfgDest);
   fs.copySync(cssSrc, cssDest);
   fs.copySync(prettierSrc, prettierDest);
+
+  handleFramework(projectDir, framework);
 };
